77f61cc5d60d2e065e513862b668a82528c2249d
Dan Golding <dan.golding@oracle.com>
2020-05-07 00:37:16 +0100

    Removed altair test scrip

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,16 +1,72 @@
+from typing import Iterable, Optional
+
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.axes._subplots import Axes
 
 from covid19_dashboard.data_adaptor import DataAdaptor, ValueType
 
-data_adaptor = DataAdaptor()
-# countries = ["US", "Italy", "South Africa", "United Kingdom"]
-countries = ["US", "Italy", "United Kingdom"]
-start = 10
-data_country = data_adaptor.get_time_series_for_country(ValueType.DEATHS, countries, start)
 
-fig, ax = plt.subplots()
-ax.plot(data_country.index.days, data_country[countries])
-ax.set_yscale("log")
-ax.set_xlim([-1, data_country.index.days.max()])
-ax.set_ylim([start, data_country[countries].max().max()])
+def plot_data_since(data_adaptor: DataAdaptor,
+                    type_: ValueType,
+                    countries: Iterable[str],
+                    ax: Axes,
+                    start: Optional[int] = None,
+                    legend: bool = False) -> None:
+
+    data_country = data_adaptor.get_time_series_for_country(type_, countries, start)
+
+    time_data = data_country.index.days if start else data_country.index
+    ax.plot(time_data, data_country[countries])
+    if legend:
+        ax.legend(countries)
+    ax.set_yscale("log")
+    if start:
+        ax.set_xlim(-1.0, data_country.index.days.max())
+    ax.set_ylim(start, data_country.max().max())
+    ax.set_ylabel(type_.value)
+    if start:
+        ax.set_xlabel(f"Days since {start} {type_.value}")
+    else:
+        ax.set_xlabel("Date")
+
+
+def plot_delta_since(data_adaptor: DataAdaptor,
+                     type_: ValueType,
+                     countries: Iterable[str],
+                     ax: Axes,
+                     rolling: int = 1,
+                     start: Optional[int] = None,
+                     legend: bool = False) -> None:
+
+    data_country = data_adaptor.get_time_delta_for_country(type_, countries, start, rolling)
+    data_country[data_country <= 0] = np.NAN
+    if start:
+        data_country = data_country[data_country.index.days >= 0]
+
+    time_data = data_country.index.days if start else data_country.index
+    ax.plot(time_data, data_country[countries])
+    if legend:
+        ax.legend(countries)
+    ax.set_yscale("log")
+    ax.set_ylim(data_country.min().min(), data_country.max().max())
+    ylabel = f"{type_.value} per day"
+    if rolling > 1:
+        ylabel += f" ({rolling} day moving average)"
+    ax.set_ylabel(ylabel)
+    if start:
+        ax.set_xlabel(f"Days since {start} {type_.value}")
+    else:
+        ax.set_xlabel("Date")
+
+
+data_adaptor = DataAdaptor()
+countries = ["US", "Italy", "United Kingdom", "Spain", "Netherlands", "Israel", "Sweden"]
+
+fig, ax = plt.subplots(1, 2)
+
+plot_data_since(data_adaptor, ValueType.DEATHS, countries, ax[0], start=100, legend=True)
+plot_delta_since(data_adaptor, ValueType.DEATHS, countries, ax[1], rolling=7, start=100, legend=True)
+
+plt.tight_layout()
 plt.show()
